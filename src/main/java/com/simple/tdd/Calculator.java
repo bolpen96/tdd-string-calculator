19f4478b08d4e4323815b90de5d1b8a48f75cb4a
Pie <usd_zero@naver.com>
2018-02-07 15:04:15 +0900

    init commit

---
+++ src/main/java/com/simple/tdd/Calculator.java
@@ -0,0 +1,17 @@
+package java.com.simple.tdd;
+
+public class Calculator {
+    public int add(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+    public int multiply(int number1, int number2){
+        return (number1*number2);
+    }
+    public int division(int number1, int number2){
+        return number1/number2 ;
+    }
+}
